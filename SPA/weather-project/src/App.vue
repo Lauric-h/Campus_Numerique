a1538ff359565a1ad9874c3cb40f6653fc8de334
lauric-h <h.lauric@outlook.com>
2021-04-22 13:48:10 +0200

    Fin etape 3

---
+++ SPA/weather-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Home />
+    <CitiesList />
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+import CitiesList from './components/CitiesList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+    CitiesList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ SPA/weather-project/src/components/CitiesList.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Météo des villes</h1>
+
+
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'CitiesList'
+}
+</script>
